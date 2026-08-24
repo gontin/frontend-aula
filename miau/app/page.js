a03164eb56359e83220ff3fef10467158068f0b4
Gustavo Silveira Nicoletti <gus.nicoletti@gmail.com>
2026-08-24 19:16:57 -0300

    Centralizando o minibio

--- miau/app/page.js
+++ miau/app/page.js
@@ -2,7 +2,7 @@ import Minibio from "@/components/Minibio";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <h1>Mini bio</h1>
       <Minibio />
     </div>
